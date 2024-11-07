c1b01998b109a71193072873eca6b244e5551f07
Ran Wang <124749510+RanKyoto@users.noreply.github.com>
2024-11-07 17:04:51 +0900

    upload

--- models.py
+++ models.py
@@ -121,6 +121,39 @@ class L2_Dynamics(Stable_Dynamics):
         V.sum().backward()
         grad_V = x.grad.unsqueeze(dim=2)
         
+        # please modify W for different dynamics, this is only for van der Pol
+        W = (x[:,0] ** 2).reshape(-1,1,1) + grad_V[:,1,:].reshape(-1,1,1)
+
+        criterion = grad_V.transpose(1,2) @ (f0 + g @ alpha) + W
+        fs = -  criterion / (th.norm(grad_V,2,dim=1,keepdim=True) ** 2 + 1e-6) * grad_V
+        
+        mask = criterion <= 0
+        f = th.where(mask, f0, f0 + fs)
+
+        del x
+        return f, g, alpha
+
+class HJI_Dynamics(Stable_Dynamics):
+    def __init__(self,
+                 state_dim: int,
+                 action_dim: int,
+                 net_arch: List[int] = [32,32],
+                 activation_fn: Type[nn.Module] = nn.ReLU,
+                 device: Union[th.device, str] = "auto"
+                 ):
+        super().__init__(state_dim, action_dim, net_arch, activation_fn, device)
+       
+        
+    def forward(self, x:th.Tensor):
+        f0 = (self.f_hat(x) - self.f_hat(self.x_stable)).unsqueeze(dim=2)
+        alpha = (self.alpha_hat(x) - self.alpha_hat(self.x_stable)).unsqueeze(dim=2)
+        g = th.stack([gi(x).reshape(-1, self.state_dim,1) for gi in self.g],dim=2).squeeze(dim=3)
+        
+        V = self.lyapunov_function.forward(x)
+        V.sum().backward()
+        grad_V = x.grad.unsqueeze(dim=2)
+        
+        # please modify W for different dynamics, this is only for van der Pol
         W = (x[:,0] ** 2).reshape(-1,1,1) + grad_V[:,1,:].reshape(-1,1,1)
 
         criterion = grad_V.transpose(1,2) @ (f0 + g @ alpha) + W
